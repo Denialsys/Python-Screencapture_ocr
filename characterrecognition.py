56e6e10b5d86bda289dacc27480d18cb3abd4fbb
Jenver <denialsys@gmail.com>
2023-04-17 21:12:22 +0800

    added the paragraph param

--- characterrecognition.py
+++ characterrecognition.py
@@ -1,10 +1,12 @@
 import easyocr
-
+import os
 class CharacterRecognition:
 
-    def __init__(self, language):
+    def __init__(self, language, is_gpu_enabled=False):
         self.__language = language
-        self.reader = easyocr.Reader([self.__language])
+        self.reader = easyocr.Reader([self.__language], gpu=is_gpu_enabled)
+        self.cwd = os.getcwd()
+        self.outputfile_raw = os.path.join(os.getcwd(), 'raw.png')
 
 
     def get_text(self, image):
@@ -18,9 +20,8 @@ class CharacterRecognition:
 
         image.save('D:\\Desktop\\file_name.png')
         # Read the text from the screenshot
-        result = self.reader.readtext('D:\\Desktop\\file_name.png')
-        print(result)
+        result = self.reader.readtext('D:\\Desktop\\file_name.png', paragraph=True)
         # Extract the Japanese text from the OCR result
-        extracted_text = ''.join([item[1] for item in result if item[0][1] == self.__language])
+        extracted_text = ''.join(item[1] for item in result)
         return extracted_text
 
